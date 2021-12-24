2cc74662e9dffbaa5fc885d542a64a2f5236fade
Hammad Asghar <86531993+ihammadasghar@users.noreply.github.com>
2021-12-24 17:08:11 +0000

    Created save function in Persistence Controller

--- controllers/PersistenceController.py
+++ controllers/PersistenceController.py
@@ -4,3 +4,27 @@
 
 from models import Board
 from models import PlayerRecord as PR
+
+def save_game(filename):
+    FILEPATH = f"{filename}"
+    with open(FILEPATH, mode="w") as file:
+        #  Save player records
+        player_records = PR.all()
+        for player in player_records:
+            file.write(f"{player['name']};{player['played']};{player['won']};{player['drawn']};{player['lost']}\n")
+
+        file.write("\n")
+
+        #  Save board state
+        board = Board.board
+        for player in board.items():
+            name = player[0]
+            pockets = player[1]
+            line = name + ";" 
+            for i in range(len(pockets)): 
+                if i != len(pockets)-1:  #  We dont want to include comma after the last element
+                    line += str(pockets[i]) + ","
+                else:
+                    line += str(pockets[i]) + "\n"
+            
+            file.write(line)
