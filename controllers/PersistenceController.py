e7adc3fd138f363389c60df50a22af9cd065c6fc
Hammad Asghar <86531993+ihammadasghar@users.noreply.github.com>
2021-12-26 16:36:43 +0000

    Updated load_game method

--- controllers/PersistenceController.py
+++ controllers/PersistenceController.py
@@ -2,46 +2,43 @@
 # vai ser implementado neste ficheiro (esta mensagem vai ser 
 # removida antes de submeter o projeto)
 
+import os
+from controllers.GameController import create_player_instance
 from models import Board
 from models import PlayerRecord as PR
 
 
 def load_game(filename):
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
+    filepath = f"./saved/{filename}"
+    if os.path.exists(filepath):
+        with open(filepath, "r", encoding="utf-8") as file:
             lines = file.readlines()
             loaded_player_records = []
             loaded_board = {}
-            checking = "records"
+            checking_player_records = True
             for line in lines:
                 if line != "\n":
-                    if checking == "records":
-                        player = line.split(";")
-
-                        for i in range(len(player)):
-                            try:
-                                player[i] = int(player[i])
-                            except ValueError:
-                                pass
-
-                        new_records.append({"name": player[0],"played": player[1],"won": player[2],"drawn": player[3],"lost": player[4]})
+                    if checking_player_records:
+                        player = line[:-1].split(";")
+                        instance = create_player_instance(*player)
+                        loaded_player_records.append(instance)
 
                     else:
                         item = line.split(";")
                         name = item[0]
-                        pockets = item[1].split(",")
-                        for i in range(len(pockets)):
-                            pockets[i] = int(pockets[i])
-                        new_board.update({name: pockets})
+                        pockets = item[1][:-2].split(",")
+                        pockets = [int(p) for p in pockets]
+                        loaded_board[name] = pockets
 
                 else:
-                    checking = "board"
+                    checking_player_records = False
 
-            Board.board = new_board
-            PR.player_records = new_records
+            Board.set(loaded_board)
+            PR.set(loaded_player_records)
+            print(Board.get())
+            print(PR.all())
             return True
-            
-    except FileNotFoundError:
+    else:
         return False
 
       
